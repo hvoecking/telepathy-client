2ff88ab4c9ab01bd3de7b9f384dbdb43b98ddc1b
Heye Voecking <heye.voecking@gmail.com>
2017-04-29 15:48:24 +0200

    Clean app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { ErrorHandler } from '@angular/core';
+import { IonicApp } from 'ionic-angular';
+import { IonicErrorHandler } from 'ionic-angular';
+import { IonicModule } from 'ionic-angular';
+import { NgModule } from '@angular/core';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
@@ -10,21 +13,26 @@ import { HomePage } from '../pages/home/home';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
-  bootstrap: [IonicApp],
+  bootstrap: [
+    IonicApp,
+  ],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    {
+      provide: ErrorHandler,
+      useClass: IonicErrorHandler,
+    },
+  ],
 })
 export class AppModule {}
