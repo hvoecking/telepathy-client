d1112bd2944b55ae9b569a1d6247bb52a2ceac72
Heye Voecking <heye.voecking@gmail.com>
2017-04-29 16:17:14 +0200

    Implement basic home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    private navCtrl: NavController,
+    private platform: Platform,
+  ) {
+    //
   }
 
 }
